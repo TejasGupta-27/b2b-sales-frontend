65177feed0c30fce7942ce355c1047b6e7a39801
TejasGupta-27 <b22cs093@iitj.ac.in>
2025-07-17 22:47:34 +0530

    Metric Updates

--- components/recorder-js.d.ts
+++ components/recorder-js.d.ts
@@ -1,1 +1,9 @@
-declare module 'recorder-js'; 
+declare class Recorder {
+  constructor(audioContext: AudioContext, config?: any);
+  init(stream: MediaStream): Promise<void>;
+  start(): Promise<void>;
+  stop(): Promise<{ blob: Blob, buffer: Float32Array[] }>;
+  // Add any other methods you use if needed
+}
+
+export default Recorder; 
